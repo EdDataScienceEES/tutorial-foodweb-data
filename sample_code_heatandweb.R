b7303c44e42b2724faef6511ec30bb88ef489da3
keenmustard <s2472885@ed.ac.uk>
2024-11-21 06:41:14 +0000

    Changed names of some variables

--- sample_code_heatandweb.R
+++ sample_code_heatandweb.R
@@ -57,26 +57,26 @@ list(Nodes_Not_in_Resources = nodes_not_in_resources,
 # Calculate biomass (M * N) for prey and keep only mass for predators
 node_properties <- node_properties %>%
   mutate(
-    Prey_Biomass = M * N,  # Prey biomass includes mass and density
-    Predator_Mass = M      # Predator biomass is just the mass
+    prey_biomass = M * N,  # Prey biomass includes mass and density
+    predator_mass = M      # Predator biomass is just the mass
   ) %>%
-  select(node, Prey_Biomass, Predator_Mass)  # Keep only necessary columns
+  select(node, prey_biomass, predator_mass)  # Keep only necessary columns
 
 # Joining trophic_links with node_properties
 trophic_links <- trophic_links %>%
   # Add prey biomass by joining on 'resource' (prey nodes)
-  left_join(node_properties %>% select(node, Prey_Biomass), by = c("resource" = "node")) %>%
+  left_join(node_properties %>% select(node, prey_biomass), by = c("resource" = "node")) %>%
   # Add predator mass by joining on 'consumer' (predator nodes)
-  left_join(node_properties %>% select(node, Predator_Mass), by = c("consumer" = "node"))
+  left_join(node_properties %>% select(node, predator_mass), by = c("consumer" = "node"))
 
 # Calculate and normalise interaction strength
 food_web <- trophic_links %>% # Rename the data frame as it not only contains trophic links now
   group_by(consumer) %>%  # Normalised strengths are based on single predator 
   mutate(
     # Calculate interaction strength (I)
-    Interaction_Strength = Prey_Biomass / Predator_Mass,  
+    interaction_strength = prey_biomass / predator_mass,  
     # Normalize interaction strength 
-    Normalized_Strength = Interaction_Strength / sum(Interaction_Strength, na.rm = TRUE)
+    normalized_strength = interaction_strength / sum(interaction_strength, na.rm = TRUE)
   ) %>%
   ungroup()  # Ungroup after calculations to avoid accidental grouping later 
 
@@ -93,7 +93,7 @@ str(food_web_plot)
 # Use ggraph to visualize the food web
 food_web_network <- ggraph(food_web_plot) +  # Fruchterman-Reingold layout
   geom_node_point() +  # Nodes as circles
-  geom_edge_link(aes(alpha = Normalized_Strength)) +  # Edges scaled by strength
+  geom_edge_link(aes(alpha = normalized_strength)) +  # Edges scaled by strength
   geom_node_text(aes(label = name), repel = TRUE, size = 4)   # Node labels
 
 # Print the food web plot
@@ -102,7 +102,7 @@ print(food_web_network)
 # Use ggraph to visualize the food web again, but make it prettier
 food_web_network <- ggraph(food_web_plot, layout = 'fr') +  # Fruchterman-Reingold layout tends to cluster interacting species together
   geom_edge_link(
-    aes(color = Normalized_Strength), # Plot edges (links) and colour them based on normalized strength
+    aes(color = normalized_strength), # Plot edges (links) and colour them based on normalized strength
     arrow = arrow(length = unit(5, "mm"), type = "closed")  # Add arrowheads to show who's predator who's prey
   ) +  
   scale_edge_color_gradient(low = "green", high = "red", name = "Normalized Interaction Strength") +  # Customise normalized strength gradient 
@@ -128,7 +128,7 @@ ggsave("food_web_plot.jpg", plot = food_web_network, width = 15, height = 15, dp
 # 4.	Visualize biomass flow as heatmap with ggplot2
 
 # Build a biomass flow heatmap (predator on horizontal axis, prey on vertical axis, colour represents normalized interaction strength)
-heatmap_plot <- ggplot(food_web, aes(x = consumer, y = resource, fill = Normalized_Strength)) +
+heatmap_plot <- ggplot(food_web, aes(x = consumer, y = resource, fill = normalized_strength)) +
   geom_tile() + # Add a tile plot layer as the heatmap
   labs( # Add the title and labels
     title = "Food Web Heatmap of Interaction Strength",
@@ -139,7 +139,7 @@ heatmap_plot <- ggplot(food_web, aes(x = consumer, y = resource, fill = Normaliz
 print(heatmap_plot)
 
 # Improve the heatmap
-heatmap_plot <- ggplot(food_web, aes(x = consumer, y = resource, fill = Normalized_Strength)) +
+heatmap_plot <- ggplot(food_web, aes(x = consumer, y = resource, fill = normalized_strength)) +
   geom_tile(color = "#4d4d4d", size = 0.2) + # Colour the border so we can easily count the number of tiles
   scale_fill_gradient(low = "green", high = "red",# Change to a more distinctive colour gradient
                       name = "Normalized Interaction Strength")+  # Set the legend title here
